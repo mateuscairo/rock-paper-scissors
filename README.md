175e66a53269509e3be057f7ecbafc9f42b4ee32
mateuscairo <mateus.cairo@gmail.com>
2022-11-13 19:57:38 -0300

    Finish game rock, paper and scissors. Tested and played in to browser's console.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+First project from The Odin's Project using Javascript. Along this project, I could explore about all basics of Javascript - declaring variables, using functions, conditionals and loops. The project itself its quite simple, but I could develop a functional game with simple concepts that's played in the browser console.
--- js/code.js
+++ js/code.js
@@ -2,29 +2,35 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * options.length);
-    return computerChoice;
+    return options[computerChoice];
 }
 
+function getPlayerChoice (){
+    let playerChoice = prompt("Choose: Rock, paper or scissors?");
+    return playerChoice;
+} 
+
 function playRound (playerSelection, computerSelection) {
-    let playerSelection = prompt("Choose: Rock, paper or scissors?");
-    let computerSelection = getComputerChoice();
+
+    playerSelection = getPlayerChoice().toLowerCase();
+    computerSelection = getComputerChoice();
 
     if (playerSelection === "rock") {
         if (computerSelection === "rock"){
             console.log("Computer selected rock, it's a tie!");
-        } else if (computerSelection === "papper"){
+        } else if (computerSelection === "paper"){
             console.log("Computer selected papper, you loose!");
-        } else (computerSelection === "scissors"){
+        } else if(computerSelection === "scissors"){
             console.log("Computer selected scissors, you won!");
         }
     }
 
-    if (playerSelection === "papper") {
+    if (playerSelection === "paper") {
         if (computerSelection === "rock"){
             console.log("Computer selected rock, you won!");
-        } else if (computerSelection === "papper"){
+        } else if (computerSelection === "paper"){
             console.log("Computer selected papper, it's a tie!");
-        } else (computerSelection === "scissors"){
+        } else if (computerSelection === "scissors"){
             console.log("Computer selected scissors, you loose!");
         }
     }
@@ -32,11 +38,17 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === "scissors") {
         if (computerSelection === "rock"){
             console.log("Computer selected rock, you loose!");
-        } else if (computerSelection === "papper"){
-            console.log("Computer selected papper, you won!");
-        } else (computerSelection === "scissors"){
+        } else if (computerSelection === "paper"){
+            console.log("Computer selected paper, you won!");
+        } else if (computerSelection === "scissors"){
             console.log("Computer selected scissors, it's a tie!");
         }
     }
 
+}
+
+let param1 , param2 = undefined;
+
+for (let i = 0; i < 5; i++) {
+    playRound(param1,param2);
 }
