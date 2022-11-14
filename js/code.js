dd0bab26400341d4a92b57a6b2bea2df3940c061
mateuscairo <mateus.cairo@gmail.com>
2022-11-14 05:34:46 -0300

    Add score to the end of each round and tells the winner.

--- js/code.js
+++ js/code.js
@@ -20,26 +20,32 @@ function playRound (playerSelection, computerSelection) {
             console.log("Computer selected rock, it's a tie!");
         } else if (computerSelection === "paper"){
             console.log("Computer selected papper, you loose!");
+            scoreComputer++;
         } else if(computerSelection === "scissors"){
             console.log("Computer selected scissors, you won!");
+            scorePlayer++;
         }
     }
 
     if (playerSelection === "paper") {
         if (computerSelection === "rock"){
             console.log("Computer selected rock, you won!");
+            scorePlayer++;
         } else if (computerSelection === "paper"){
             console.log("Computer selected papper, it's a tie!");
         } else if (computerSelection === "scissors"){
             console.log("Computer selected scissors, you loose!");
+            scoreComputer++;
         }
     }
 
     if (playerSelection === "scissors") {
         if (computerSelection === "rock"){
             console.log("Computer selected rock, you loose!");
+            scoreComputer++;
         } else if (computerSelection === "paper"){
             console.log("Computer selected paper, you won!");
+            scorePlayer++;
         } else if (computerSelection === "scissors"){
             console.log("Computer selected scissors, it's a tie!");
         }
@@ -49,6 +55,20 @@ function playRound (playerSelection, computerSelection) {
 
 let param1 , param2 = undefined;
 
+let scorePlayer = 0
+, scoreComputer = 0;
+
 for (let i = 0; i < 5; i++) {
     playRound(param1,param2);
+    console.log(`Score:\nPlayer:${scorePlayer}\nComputer:${scoreComputer}`);
+}
+
+if (scoreComputer > scorePlayer) {
+    console.log("YOU LOOSE")
+}
+else if (scoreComputer < scorePlayer){
+    console.log("YOU WIN")
+}
+else {
+    console.log("IT'S A TIE")
 }
